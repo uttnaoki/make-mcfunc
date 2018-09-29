10c0c3ee59fed5b54503f782e0630e13e41df8d4
Naoki Kinoshita <uttnaoki@gmail.com>
2018-09-29 21:38:28 +0900

    make_drawfuncをリファクタリング

--- make_drawfunc.py
+++ make_drawfunc.py
@@ -1,9 +1,19 @@
 from PIL import Image
+import os
 import math
 import numpy as np
 import mcblock
 
-funcdir_path = 'functions'
+# 入力画像を配置するディレクトリ
+source_img_dir = 'source_img'
+# 処理の過程で作成される画像を出力するディレクトリ
+output_img_dir = 'output_pngs'
+# マイクラ用スクリプトを出力するディレクトリ
+mcfunc_dir = 'functions'
+
+# 出力画像のサイズ
+output_img_size = (100, 100)
+
 
 # カラーコード文字列をRGBのタプルに変換する．
 def cc2rgb (colorcode):
@@ -12,14 +22,8 @@ def cc2rgb (colorcode):
     b = int(colorcode[5:7], 16)
     return (r,g,b)
 
-def resize_img (filename, size_tuple):
-    # 既存ファイルを readモードで読み込み
-    img = Image.open(filename, 'r')
-
-    # リサイズ。サイズは幅と高さをtupleで指定
-    return img.resize(size_tuple)
-
-def reduce_img_color (base_img, rgb_set, filename):
+# 入力画像を指定のRGBセットで再描画する
+def redraw (base_img, rgb_set, filename):
     # rgbの距離(norm)を計算するためにnumpyの配列に変換
     rgb_set = np.array(rgb_set)
     rgb_set_len = len(rgb_set)
@@ -40,18 +44,21 @@ def reduce_img_color (base_img, rgb_set, filename):
 
     # 画像の幅と高さを取得
     width, height = base_img.size
-    # 画像の各ピクセルの色を usable_rgb の色に修正し，修正後のRGBを取得
+    # 画像の各ピクセルの色を usable_rgb の色に修正し，修正後のRGBのインデックスを取得
     used_rgb_indexes = [mod_one_pixel(x, y) for y in range(height) for x in range(width)]
     # 修正後の画像を保存
     base_img.save(filename)
 
+    # 再描画に使ったRGBのインデックスを返却
     return used_rgb_indexes
 
+# マイクラ上の特定の座標にブロックを設置するコマンドを取得
 def get_command (block_name, index, canvas_size, transform):
     x = index%canvas_size[0] + transform['x']
     y = -(index//canvas_size[1]) + transform['y']
     return 'setblock ~{0} ~{1} ~ {2}'.format(x, y, block_name)
 
+# 画像をマイクラ上で描画するためのコマンドセットを取得
 def get_command_set (block_names, selected_block_indexes, canvas_size):
     transform = {
         'x': -(canvas_size[0]//2),
@@ -59,27 +66,37 @@ def get_command_set (block_names, selected_block_indexes, canvas_size):
     }
     return [get_command(block_names[selected_block_indexes[i]], i, canvas_size, transform) for i in range(len(selected_block_indexes))]
 
-def main (source_img_name):
+def main (source_img_path):
+    # 入力画像をリサイズ
+    img = Image.open(source_img_path, 'r')
+    resized_img = img.resize(output_img_size)
+    # RGBに変換
+    rgb_img = resized_img.convert('RGB')
+
     # 羊毛ブロックの名前(ID)を抽出
     wool_names = [name for name in mcblock.wool_colorcode]
     # 羊毛ブロックのRGBを抽出
     usable_rgb_set = [cc2rgb(cc) for cc in mcblock.wool_colorcode.values()]
 
-    # 出力画像のサイズ
-    output_img_size = (100, 100)
-
-    # 入力画像をリサイズ
-    resized_img = resize_img(source_img_name, output_img_size)
-    # RGBに変換
-    rgb_img = resized_img.convert('RGB')
-
-    # usable_rgb_set で表現した画像を保存し，使われたRGBとそのindexを取得．
-    used_rgb_indexes = reduce_img_color(rgb_img, usable_rgb_set, 'wool_mode.png')
-
+    # usable_rgb_set で表現した画像を保存し，使われたRGBのindexを取得
+    used_rgb_indexes = redraw(rgb_img, usable_rgb_set, '{0}/wool_mode.png'.format(output_img_dir))
+    # 再描画後の画像をマイクラで描画するためのコマンドセットを取得
     command_set = get_command_set(wool_names, used_rgb_indexes, output_img_size)
-    funcfile_path = '{0}/draw_pikachu.mcfunction'.format(funcdir_path)
-    with open(funcfile_path, 'w', encoding='utf-8') as f:
+
+    # 画像名を取得
+    source_img_name = source_img_path.split('/')[-1]
+    # 画像名の拡張子じゃない方を取得
+    source_img_basename = source_img_name.split('.')[0]
+    # 出力するマイクラ用スクリプトのパス名を取得
+    mcfunc_path = '{0}/draw_{1}.mcfunction'.format(mcfunc_dir, source_img_basename)
+    # マイクラ用スクリプトを出力
+    with open(mcfunc_path, 'w', encoding='utf-8') as f:
         [f.write('{0}\n'.format(c)) for c in command_set]
 
 if __name__ == '__main__':
-    main('pikachu.png')
+    # 入力画像名を取得
+    img_names = os.listdir(source_img_dir)
+
+    # 各入力画像に対して処理を実行
+    for img_name in img_names:
+        main('{0}/{1}'.format(source_img_dir, img_name))
